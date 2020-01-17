3f752e59f5ab1725b2f4bb0c45f3e9eb45fb071a
Zach Silveira <zackify@gmail.com>
2020-01-16 23:02:46 -0800

    Add try catch to pem handler

--- server/src/attestation/devices/devices.ts
+++ server/src/attestation/devices/devices.ts
@@ -4,11 +4,17 @@ import { Certificate } from "@fidm/x509";
 const devices = [yubikey];
 
 export const deviceDataFromPEM = (pem: string) => {
-  const parsedPem = Certificate.fromPEM((pem as unknown) as Buffer);
+  try {
+    const parsedPem = Certificate.fromPEM((pem as unknown) as Buffer);
 
-  // return the one that finds device data inside the pem
-  for (let device of devices) {
-    let deviceData = device(parsedPem);
-    if (deviceData) return deviceData;
+    // return the one that finds device data inside the pem
+    for (let device of devices) {
+      let deviceData = device(parsedPem);
+      if (deviceData) return deviceData;
+    }
+  } catch (e) {
+    console.log(pem);
+    console.error(e);
+    return {};
   }
 };
